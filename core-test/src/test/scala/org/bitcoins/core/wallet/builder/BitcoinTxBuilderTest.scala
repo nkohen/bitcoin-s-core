8a7a835a052a225838f175329f8c3effd626ba4b
nkohen <nadavk25@gmail.com>
2020-01-21 15:57:41 -0600

    Cleanup after rebase

--- core-test/src/test/scala/org/bitcoins/core/wallet/builder/BitcoinTxBuilderTest.scala
+++ core-test/src/test/scala/org/bitcoins/core/wallet/builder/BitcoinTxBuilderTest.scala
@@ -1,13 +1,7 @@
 package org.bitcoins.core.wallet.builder
 
 import org.bitcoins.core.config.TestNet3
-import org.bitcoins.core.crypto.{
-  BaseTxSigComponent,
-  DoubleSha256DigestBE,
-  ECPrivateKey,
-  WitnessTxSigComponentP2SH,
-  WitnessTxSigComponentRaw
-}
+import org.bitcoins.core.crypto.{DoubleSha256DigestBE, ECPrivateKey}
 import org.bitcoins.core.currency._
 import org.bitcoins.testkit.core.gen.{
   ChainParamsGenerator,
@@ -23,15 +17,11 @@ import org.bitcoins.core.wallet.fee.SatoshisPerVirtualByte
 import org.bitcoins.core.wallet.utxo.{
   BitcoinUTXOSpendingInfoFull,
   ConditionalPath,
-  LockTimeSpendingInfoFull,
-  UTXOSpendingInfo
+  LockTimeSpendingInfoFull
 }
 import org.bitcoins.core.wallet.fee.SatoshisPerByte
 import org.bitcoins.core.config.RegTest
-import org.bitcoins.core.policy.Policy
-import org.bitcoins.core.script.PreExecutionScriptProgram
 import org.bitcoins.core.script.constant.ScriptNumber
-import org.bitcoins.core.script.interpreter.ScriptInterpreter
 import org.bitcoins.core.util.BitcoinScriptUtil
 import org.bitcoins.core.wallet.builder.BitcoinTxBuilder.UTXOMap
 import org.bitcoins.testkit.Implicits._
@@ -503,63 +493,6 @@ class BitcoinTxBuilderTest extends BitcoinSAsyncTest {
     recoverToSucceededIf[IllegalArgumentException](
       txBuilderF.flatMap(_.unsignedTx)
     )
-  }
-
-  def verifyScript(tx: Transaction,
-                   utxos: Vector[UTXOSpendingInfo]): Boolean = {
-    val programs: Vector[PreExecutionScriptProgram] =
-      tx.inputs.zipWithIndex.toVector.map {
-        case (input: TransactionInput, idx: Int) =>
-          val outpoint = input.previousOutput
-
-          val creditingTx =
-            utxos.find(u => u.outPoint.txId == outpoint.txId).get
-
-          val output = creditingTx.output
-
-          val spk = output.scriptPubKey
-
-          val amount = output.value
-
-          val txSigComponent = spk match {
-            case witSPK: WitnessScriptPubKeyV0 =>
-              val o = TransactionOutput(amount, witSPK)
-              WitnessTxSigComponentRaw(tx.asInstanceOf[WitnessTransaction],
-                                       UInt32(idx),
-                                       o,
-                                       Policy.standardFlags)
-            case _: UnassignedWitnessScriptPubKey => ???
-            case x @ (_: P2PKScriptPubKey | _: P2PKHScriptPubKey |
-                _: P2PKWithTimeoutScriptPubKey | _: MultiSignatureScriptPubKey |
-                _: WitnessCommitment | _: CSVScriptPubKey |
-                _: CLTVScriptPubKey | _: ConditionalScriptPubKey |
-                _: NonStandardScriptPubKey | EmptyScriptPubKey) =>
-              val o = TransactionOutput(CurrencyUnits.zero, x)
-              BaseTxSigComponent(tx, UInt32(idx), o, Policy.standardFlags)
-
-            case p2sh: P2SHScriptPubKey =>
-              val p2shScriptSig =
-                tx.inputs(idx).scriptSignature.asInstanceOf[P2SHScriptSignature]
-              p2shScriptSig.redeemScript match {
-
-                case _: WitnessScriptPubKey =>
-                  WitnessTxSigComponentP2SH(
-                    transaction = tx.asInstanceOf[WitnessTransaction],
-                    inputIndex = UInt32(idx),
-                    output = output,
-                    flags = Policy.standardFlags)
-
-                case _ =>
-                  BaseTxSigComponent(tx,
-                                     UInt32(idx),
-                                     output,
-                                     Policy.standardFlags)
-              }
-          }
-
-          PreExecutionScriptProgram(txSigComponent)
-      }
-    ScriptInterpreter.runAllVerify(programs)
   }
 
   private def outputGen(outputs: Gen[Seq[BitcoinUTXOSpendingInfoFull]]) = {
